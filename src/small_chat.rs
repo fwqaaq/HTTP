78b5fa8bb2c0cdb37127dec78a6842e0b47d1fb2
fwqaaq <fwqaaq@gmail.com>
2023-11-03 19:13:09 +0800

    rewrite small_chat

--- src/small_chat.rs
+++ src/small_chat.rs
@@ -1,10 +1,9 @@
 use std::io::{Read, Write};
-use std::net::{SocketAddr, TcpListener, TcpStream};
-use std::sync::atomic::Ordering::Relaxed;
-use std::sync::{atomic::AtomicI16, Arc, Mutex};
+use std::net::{TcpListener, TcpStream};
+use std::sync::atomic::{AtomicI16, Ordering};
+use std::sync::{Arc, Mutex};
 use std::{collections::HashMap, thread};
 
-// const MAX_CLIENTS: u16 = 1000;
 const SERVER_PORT: u16 = 7711;
 
 struct Client {
@@ -13,93 +12,99 @@ struct Client {
 }
 
 struct Chat {
-    clients: HashMap<i16, Client>,
+    clients: Arc<Mutex<HashMap<i16, Client>>>,
     next_id: AtomicI16,
 }
 
 impl Chat {
     fn new() -> Self {
         Self {
-            clients: HashMap::new(),
+            clients: Arc::new(Mutex::new(HashMap::new())),
             next_id: AtomicI16::new(0),
         }
     }
 
-    fn add_client(&mut self, stream: TcpStream) -> i16 {
-        let prev_id = self.next_id.fetch_add(1, Relaxed);
-        let client = Client {
-            stream,
-            nick: format!("user:{}", prev_id),
-        };
-        self.clients.insert(prev_id, client);
-        prev_id
+    fn add_client(&self, stream: TcpStream) -> i16 {
+        let id = self.next_id.fetch_add(1, Ordering::Relaxed);
+        let mut clients = self.clients.lock().unwrap();
+        clients.insert(
+            id,
+            Client {
+                stream,
+                nick: id.to_string(),
+            },
+        );
+        id
     }
 
-    fn remove_client(&mut self, id: i16) {
-        self.clients.remove(&id);
+    fn get_nick(&self, id: i16) -> String {
+        let clients = self.clients.lock().unwrap();
+        clients.get(&id).unwrap().nick.clone()
     }
 
-    fn broadcast(&mut self, message: &str, excluded: i16) {
-        for (id, client) in self.clients.iter_mut() {
+    fn remove_client(&self, id: i16) {
+        let mut clients = self.clients.lock().unwrap();
+        clients.remove(&id);
+    }
+
+    fn broadcast(&self, message: &str, excluded: i16) {
+        let mut clients = self.clients.lock().unwrap();
+        for (id, client) in clients.iter_mut() {
             if *id == excluded {
                 continue;
             }
-            client
-                .stream
-                .write_fmt(format_args!("{}\n", message))
-                .unwrap();
+            let _ = client.stream.write(message.as_bytes());
+        }
+    }
+}
+
+fn handle_client(chat: Arc<Chat>, mut stream: TcpStream, id: i16) {
+    let welcome = "Welcome to chat! Please pick a nickname.\n";
+    let _ = stream.write(welcome.as_bytes());
+
+    let mut buffer = [0; 512];
+    loop {
+        let n = stream.read(&mut buffer).unwrap();
+
+        if n == 0 {
+            println!("Client {} disconnected", id);
+            chat.remove_client(id);
+            chat.broadcast(&format!("Client {} left", id), id);
+            break;
+        }
+        let message = String::from_utf8_lossy(&buffer[..n]).to_string();
+        let parts = message.splitn(2, ' ').collect::<Vec<&str>>();
+
+        match parts[0] {
+            "/nick" if parts.len() > 1 => {
+                let new_nick = parts[1].trim().to_string();
+
+                let mut clients = chat.clients.lock().unwrap();
+                let client = clients.get_mut(&id).unwrap();
+                client.nick = new_nick.clone();
+                drop(clients); // Release lock
+                chat.broadcast(&format!("Client {} is now known as {}", id, new_nick), id);
+            }
+            _ => {
+                let msg = format!("user {}: {}", chat.get_nick(id), message);
+                chat.broadcast(&msg, id);
+            }
         }
     }
 }
 
 fn main() {
-    let socket_addr = SocketAddr::from(([0, 0, 0, 0], SERVER_PORT));
-    let listener = TcpListener::bind(socket_addr).unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7711").unwrap();
+    let chat = Arc::new(Chat::new());
 
-    println!("Server listening on port {}", SERVER_PORT);
+    println!("Chat server listening on port {}", SERVER_PORT);
 
-    let chat = Arc::new(Mutex::new(Chat::new()));
     for stream in listener.incoming() {
-        let mut stream = stream.expect("Failed to unwrap stream");
-        let clone_stream = stream.try_clone().expect("Failed to clone stream");
-
-        let chat_clone = chat.clone();
-        let mut chat = chat.lock().unwrap();
-        let id = chat.add_client(clone_stream);
-
+        let stream = stream.unwrap();
+        let id = chat.add_client(stream.try_clone().unwrap());
         println!("Client {} connected", id);
 
-        chat.broadcast(&format!("{} joined", id), id);
-
-        let welcome = "Welcome to Rust Chat! Use /nick <newnick> to change your nickname.\n";
-        // Connect to the client
-        stream
-            .write_all(welcome.as_bytes())
-            .expect("Failed to write to client");
-
-        let mut buffer = [0; 512];
-
-        thread::spawn(move || loop {
-            let n = stream.read(&mut buffer).unwrap();
-            let mut chat_clone = chat_clone.lock().unwrap();
-            if n == 0 {
-                println!("Client {} disconnected", id);
-                chat_clone.remove_client(id);
-                chat_clone.broadcast(&format!("{} left", id), id);
-                break;
-            }
-
-            let message = String::from_utf8_lossy(&buffer[..n]);
-
-            if message.starts_with("/nick") {
-                let parts = message.splitn(2, ' ').collect::<Vec<&str>>();
-                if parts.len() > 1 {
-                    let client = chat_clone.clients.get_mut(&id).unwrap();
-                    client.nick = parts[1].to_string();
-                }
-            } else {
-                chat_clone.broadcast(&format!("user {}: {}", id, message), id);
-            }
-        });
+        let chat_clone = Arc::clone(&chat);
+        thread::spawn(move || handle_client(chat_clone, stream, id));
     }
 }
